601a142c4dd3004e4b50b59575401a1b800817e9
Bitajune <beheshtibita@gmail.com>
2020-05-20 09:52:05 -0700

    practice variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 let firstName = 'Bita';
+console.log(firstName);
+
+let lastName = 'De La Rosa';
+let age = 29;
+console.log(lastName, age);
+
+let fullAge = true;
+let job = 'Teacher';
+console.log(fullAge, job);
