4247ca0602efba092fd74ce403c337b02a2a09f4
Bitajune <beheshtibita@gmail.com>
2020-05-20 11:15:30 -0700

    variable mutation

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ Variable mutation and type coercion
 
 let firstName = 'Bita';
 let age = 29;
+
+//Type coercion
 console.log(firstName + ' ' + age);
 
 let job, isMarried;
@@ -26,3 +28,12 @@ job = 'Teacher';
 isMarried = true;
 
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty nine';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried);
+
+let lastName = prompt('What is her last name?');
+console.log(firstName, lastName);
