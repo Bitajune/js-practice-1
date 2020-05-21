f80e21f1ee33d5692efbd9f038b4a3999d8a0f00
Bitajune <beheshtibita@gmail.com>
2020-05-21 11:23:19 -0700

    multiple assignments

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-// /*****************************
-// Variables and data types
-// */
+/*****************************
+Variables and data types
+*/
 
-// // let firstName = 'Bita';
-// // console.log(firstName);
+// let firstName = 'Bita';
+// console.log(firstName);
 
-// // let lastName = 'De La Rosa';
-// // let age = 29;
-// // console.log(lastName, age);
+// let lastName = 'De La Rosa';
+// let age = 29;
+// console.log(lastName, age);
 
-// // let fullAge = true;
-// // let job = 'Teacher';
-// // console.log(fullAge, job);
+// let fullAge = true;
+// let job = 'Teacher';
+// console.log(fullAge, job);
 
-// /*****************************
-// Variable mutation and type coercion
-// */
+/*****************************
+Variable mutation and type coercion
+*/
 
 // let firstName = 'Bita';
 // let age = 29;
-
-// //Type coercion
 // console.log(firstName + ' ' + age);
 
 // let job, isMarried;
 // job = 'Teacher';
 // isMarried = true;
-
 // console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried);
 
-// // Variable mutation
+// // variable mutation
+
 // age = 'twenty nine';
-// job = 'driver';
-
+// job = 'teacher';
 // alert(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried);
-
 // let lastName = prompt('What is her last name?');
 // console.log(firstName, lastName);
 
@@ -48,9 +44,9 @@ Basic operators
 // ageChris = 31;
 
 // // math operators
+
 // yearBita = year - ageBita;
 // yearChris = year - ageChris;
-
 // console.log(yearBita, yearChris);
 // console.log(year + 20);
 // console.log(year * 20);
@@ -62,6 +58,7 @@ Basic operators
 // console.log(chrisOlder);
 
 // // typeof operators
+
 // console.log(typeof chrisOlder);
 // console.log(typeof ageBita);
 // console.log(typeof 'Chris is older than Bita');
@@ -72,19 +69,35 @@ Basic operators
 Operator precedence
  */
 
-let year = 2020;
-let yearBita = 1990;
-let fullAge = 21;
+// let year = 2020;
+// let yearBita = 1990;
+// let fullAge = 21;
 
-// multiple operators
-let isFullAge = year - yearBita >= fullAge;
-console.log(isFullAge);
+// // multiple operators
 
-// grouping
-let ageBita = year - yearBita;
-let ageChris = 31;
-let average = (ageBita + ageChris) / 2;
-console.log(average);
+// let isFullAge = year - yearBita >= fullAge;
+// console.log(isFullAge);
 
-// multiple assignments
-let x, y;
+// // grouping
+
+// let ageBita = year - yearBita;
+// let ageChris = 31;
+// let average = (ageBita + ageChris) / 2;
+// console.log(average);
+
+// // multiple assignments
+
+// let x, y;
+// x = y = (3 + 5) * 4 - 6;
+// console.log(x);
+
+// x *= 2
+// console.log(x)
+// x += 10
+// console.log(x)
+// x -= 10
+// console.log(x)
+// x++;
+// console.log(x)
+// x--;
+// console.log(x);
